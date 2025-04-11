b28635b75f2bcc930c1bfb6fdab57ab3648e3862
birushandegeya <birushandegeya@gmail.com>
2025-04-11 10:32:33 +0200

    fix: Type error: Page 'src/app/(external)/trainings/page.tsx' does not match the required types of a Next.js Page

--- src/app/(external)/trainings/page.tsx
+++ src/app/(external)/trainings/page.tsx
@@ -15,19 +15,7 @@ import Link from "next/link";
 import NewsLetter from "../components/newsletter";
 import CourseModal from "@/components/course-modal";
 import { useState } from "react";
-
-export const dcgData = {
-  title: "Diplôme de Comptabilité et de Gestion (DCG)",
-  category: "Comptabilité et Finance",
-  level: "Bac+3",
-  description:
-    "Le Diplôme de Comptabilité et de Gestion (DCG) est un diplôme d’État de niveau licence (Bac+3)...",
-  modalities: ["Alternance", "En ligne", "À la carte"],
-  duration: "3 ans",
-  rncp: "RNCP35526",
-  price: "À partir de 199€",
-  link: "#",
-};
+import { dcgData } from "./data/index";
 
 const filters = [
   { label: "Niveau", value: "Tous les niveaux" },
