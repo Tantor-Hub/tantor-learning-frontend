4293774b19e41a3161fbe0451dcc74a9338f900b
birushandegeya <birushandegeya@gmail.com>
2025-04-15 21:17:18 +0200

    feat: 404 pages

---
+++ src/components/not-found.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+
+export default function NotFound() {
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center justify-center py-2 max-w-[727px] m-auto px-[20px] md:px-[111px] h-screen">
+      <div className="max-w-[505px] flex flex-col items-center text-center gap-[16px] md:gap-[28px] pb-[25px] md:pb-[50px]">
+        <h1 className="text-[56px] md:text-[96px] font-black bg-gradient-to-b from-primary to-blue-200 bg-clip-text text-transparent">
+          Oops!
+        </h1>
+        <h2 className="text-[24px] md:text-[36px] font-semibold">404 - Page non trouvée</h2>
+        <p className="text-[16px] md:text-[20px]">
+          La page que vous recherchez a peut-être été supprimée, renommée ou est temporairement
+          indisponible.
+        </p>
+      </div>
+      <Button
+        size="lg"
+        className="hover:bg-blue-500 hover:cursor-pointer transition-colors"
+        onClick={() => router.push("/")}
+      >
+        Retour à l'accueil <ArrowRightIcon />
+      </Button>
+    </div>
+  );
+}
