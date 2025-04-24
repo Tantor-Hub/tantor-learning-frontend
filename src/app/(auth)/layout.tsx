97d1997aea7a8823169735412922f7f271ea5eaa
Birusha Ndegeya <birushandegeya@gmail.com>
2025-04-24 19:02:42 +0200

    fix(auth): change <a></a> tag into Next Link tag

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function AuthLayout({
   children,
@@ -11,7 +12,7 @@ export default function AuthLayout({
       <div className="grid min-h-svh lg:grid-cols-2">
         <div className="flex flex-col gap-4 p-6 md:p-10">
           <div className="flex justify-center gap-2 md:justify-start">
-            <a href="#" className="flex items-center gap-2 font-medium">
+            <Link href="/" className="flex items-center gap-2 font-medium">
               <div className="flex h-6 w-6 items-center justify-center rounded-md">
                 <Image
                   src="/tantor-logo.svg"
@@ -22,7 +23,7 @@ export default function AuthLayout({
                 />
               </div>
               TANTOR LEARNING
-            </a>
+            </Link>
           </div>
           <div className="flex flex-1 items-center justify-center">
             <div className="w-full max-w-xs">{children}</div>
