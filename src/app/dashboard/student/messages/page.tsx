151f92f69d92be220ebba7a8e30c6e8b39b94186
birushandegeya <birushandegeya@gmail.com>
2025-04-27 19:27:56 +0200

    feat(dashboard): cours etudiant

--- src/app/dashboard/student/messages/page.tsx
+++ src/app/dashboard/student/messages/page.tsx
@@ -19,7 +19,7 @@ import { messages } from "./data";
 
 export default function Page() {
   return (
-    <div className="bg-white p-4">
+    <div className="p-4">
       <div className="flex items-center justify-between max-w-[620px] bg-[#D8D8D8] rounded-[8px] p-2 px-6 mb-4">
         <p className="bg-white rounded-[8px] p-1 px-4">Tous</p>
         <p>Archives</p>
