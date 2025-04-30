4279ebbd5374d09102dfa0d482335a54cdfd3b6f
Jomagene <semjomagene@gmail.com>
2025-04-30 15:51:00 +0200

    fix: general bg to slight grey

--- src/app/dashboard/student/components/ongoing-course.tsx
+++ src/app/dashboard/student/components/ongoing-course.tsx
@@ -6,7 +6,7 @@ import { useRouter } from "next/navigation";
 const OngoingCourse = ({ ongoing }: { ongoing: OngoingCourseTypes }) => {
   const router = useRouter();
   return (
-    <div className="p-4 flex flex-col gap-y-1.5 md:flex-row justify-between border border-border my-4">
+    <div className="p-4 flex flex-col gap-y-1.5 md:flex-row justify-between border border-border my-4  bg-white">
       <div className="flex flex-col md:flex-row gap-5 items-center">
         <picture className="bg-[#D4DAE6] rounded-full size-8 border border-[#0466C8] text-[#0466C8] flex justify-center items-center">
           {ongoing.type}
