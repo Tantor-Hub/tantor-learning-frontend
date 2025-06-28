5216d2134e36c68a85b6a084b50b9a8fe617cb54
BirushaNdegeya <birushandegeya@gmail.com>
2025-06-28 14:47:51 +0200

    feat: show students sections in a table

--- src/app/(dashboard)/student/training/page.tsx
+++ src/app/(dashboard)/student/training/page.tsx
@@ -1,77 +1,61 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { ArrowRight, BadgeEuro, Clock, GraduationCap, School } from "lucide-react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { useRouter } from "next/navigation";
 import { useGetAllTrainingsQuery } from "@/lib/apis/public/public-api";
 import { Loading } from "@/components/shared/loading";
-
-// FAKE DATA
-const dcgData = {
-  id: Math.random() * 100,
-  title: "Diplôme de Comptabilité et de Gestion (DCG)",
-  category: "Comptabilité et Finance",
-  level: "Bac+3",
-  description:
-    "Le Diplôme de Comptabilité et de Gestion (DCG) est un diplôme d'État de niveau licence (Bac+3) permettant d'acquérir des compétences complètes en gestion financière.",
-  modalities: ["Alternance", "En ligne", "À la carte"],
-  duration: "3 ans",
-  rncp: "RNCP35526",
-  price: "À partir de 199€",
-};
-
-const dcgArray = Array.from({ length: 8 }, () => ({ ...dcgData }));
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableRow,
+  TableHeader,
+} from "@/components/ui/table";
+import { EmptyState } from "@/components/shared/empty-state";
 
 export default function Page() {
   const router = useRouter();
   const { data, isLoading } = useGetAllTrainingsQuery();
   if (isLoading) return <Loading />;
 
+  if (!data?.data.list) {
+    return (
+      <EmptyState
+        icon="Calendar"
+        title="Pas de séance programmée"
+        description="Rejoignez une séance existante ou créez-en une nouvelle."
+      />
+    );
+  }
   return (
-    <div className="space-y-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {data?.data.list.map((item) => (
-          <Card key={item.id}>
-            <CardHeader>
-              <CardTitle>{item.Formation.titre}</CardTitle>
-              <CardDescription>{item.Formation.sous_titre}</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="flex flex-col gap-4">
-                <p className="text-muted-foreground">{dcgData.description}</p>
-
-                <div className="flex gap-2.5">
-                  <School className="text-foreground size-5" />
-                  <p>
-                    {dcgData.modalities.map((modality, index) => (
-                      <span key={index}>{modality + ", "}</span>
-                    ))}
-                  </p>
-                </div>
-
-                <div className="flex items-center gap-2.5">
-                  <Clock className="text-foreground size-5" />
-                  {dcgData.duration}
-                </div>
-                <div className="flex items-center gap-2.5">
-                  <GraduationCap className="text-foreground size-5" /> {""}
-                  {dcgData.rncp}
-                </div>
-                <div className="flex items-center gap-2.5">
-                  <BadgeEuro className="text-foreground size-5" /> {dcgData.price}
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-        ))}
+    <div className="border rounded-lg p-4 overflow-y-scroll">
+      <p className="text-primary text-xl font-semibold mb-4">Liste des Séances</p>
+      <div className="min-w-[1000px]">
+        <Table>
+          <TableCaption>Liste complète de mes séances</TableCaption>
+          <TableHeader className="border">
+            <TableRow>
+              <TableHead>Nom</TableHead>
+              <TableHead>Formation</TableHead>
+              <TableHead>Durée</TableHead>
+              <TableHead>Prix</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody className="border">
+            {data?.data.list.map((item) => (
+              <TableRow
+                key={item.id}
+                onClick={() => router.push(`/student/training/${item.id}`)}
+                className="hover:cursor-pointer"
+              >
+                <TableCell className="font-medium">{item.designation}</TableCell>
+                <TableCell className="text-muted-foreground">{item.Formation.titre}</TableCell>
+                <TableCell className="text-muted-foreground">{item.duree}</TableCell>
+                <TableCell className="text-muted-foreground">{item.prix} €</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
       </div>
     </div>
   );
--- src/lib/apis/secretary/seance-secretary-api.ts
+++ src/lib/apis/secretary/seance-secretary-api.ts
@@ -7,17 +7,66 @@ interface IAddSeance {
   duree: string;
   id_cours: string;
 }
+
+interface IUpdateSeance {
+  id_session: string | number; // Assuming this is a unique identifier, could be string or number
+  seance_date_on: Date | string; // Could be Date object or ISO date string
+  type_seance: string; // Type of the session
+  duree: number; // Duration (assuming in minutes or seconds)
+  id_cours: string | number; // Course ID, could be string or number
+}
+
+interface IListSeance {
+  status: number;
+  message: string;
+  data: {
+    length: number;
+    list: Array<{
+      seance_date_on: number; // Unix timestamp
+      id: number;
+      id_session: number;
+      duree: number;
+      type_seance: string; // or use union type: "onLine" | "inPerson" | "hybrid"
+      id_formation: number;
+      id_cours: number;
+      piece_jointe: null | string; // or File type if handling uploads
+      createdAt: string; // ISO date format
+      updatedAt: string; // ISO date format
+    }>;
+  };
+}
+
 // Training Secretary API
 export const seanceSecretaryApi = createApi({
   reducerPath: "seanceSecretaryApi",
   baseQuery: enhancedBaseQuery,
   tagTypes: ["SeanceSecretary"],
   endpoints: (builder) => ({
+    listSeance: builder.query<IListSeance, { idSession: string; idCours: string }>({
+      query: ({ idSession, idCours }) => `sessions/${idSession}/${idCours}/seances`,
+      providesTags: ["SeanceSecretary"],
+    }),
     addSeance: builder.mutation<void, IAddSeance>({
-      query: (body) => ({
+      query: (request) => ({
         url: "/api/sessions/session/addseance",
         method: "POST",
-        body: body,
+        body: request,
+      }),
+      invalidatesTags: ["SeanceSecretary"],
+    }),
+    updateSeance: builder.mutation<void, IUpdateSeance>({
+      query: (request) => ({
+        url: `/api/sessions/session/addseance/${request.id_session}`,
+        method: "PUT",
+        body: request,
+      }),
+      invalidatesTags: ["SeanceSecretary"],
+    }),
+    removeSeance: builder.mutation<void, { idseance: string | number }>({
+      query: (request) => ({
+        url: `/api/sessions/session/addseance/${request.idseance}`,
+        method: "DELETE",
+        body: request,
       }),
       invalidatesTags: ["SeanceSecretary"],
     }),
