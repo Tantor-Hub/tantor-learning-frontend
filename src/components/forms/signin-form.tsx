8baf9c2f775db40242be065a021c7b0175d38d5f
birushandegeya <birushandegeya@gmail.com>
2025-09-05 16:55:29 +0200

    fix: unused loading

--- src/components/forms/signin-form.tsx
+++ src/components/forms/signin-form.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
 import { GoogleIcon } from "@/components/icons/google";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,48 @@
+import { clearCredentials } from "@/features/auth/auth-slice";
+import { useState, useEffect } from "react";
+
+function useAuth() {
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+        // Get token inside useEffect to ensure it's fresh
+        const token = localStorage.getItem("authState.token");
+        console.log(token);
+
+        if (!token) {
+          setIsAuthenticated(false);
+          return;
+        }
+
+        const response = await fetch(`${baseURL}/users/user/me`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            "x-connexion-tantor": `Bearer ${token}`,
+          },
+        });
+
+        const data = await response.json();
+        console.log(JSON.stringify(data));
+        // Return true only if status is 200 and token is valid
+        // Return false for 401 (expired token) or any other error
+        setIsAuthenticated(data.status === 200 && data.data?.isTokenValid === true);
+      } catch (error) {
+        console.error("Auth check failed:", error);
+        setIsAuthenticated(false);
+        clearCredentials();
+      }
+    };
+
+    if (baseURL) {
+      checkAuthStatus();
+    }
+  }, [baseURL]);
+
+  return isAuthenticated; // Returns only boolean
+}
+
+export { useAuth };
